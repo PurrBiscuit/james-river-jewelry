55c8246838ade464c2a44a29a4ce3c3d4f85ad34
Purrbiscuit <spurr@articulate.com>
2019-03-11 16:47:39 -0400

    add HISTORIC_REWARDS table drop and create

--- creates-alters-drops/creates-alters-drops.sql
+++ creates-alters-drops/creates-alters-drops.sql
@@ -8,3 +8,15 @@ CREATE TABLE ACTIVE_REWARDS(
 	ExpiryDate date NOT NULL,
 	CONSTRAINT PK_ActiveRewardID PRIMARY KEY(ActiveRewardID)
 );
+
+DROP TABLE IF EXISTS HISTORIC_REWARDS;
+
+CREATE TABLE HISTORIC_REWARDS(
+	HistoricRewardID uniqueidentifier NOT NULL,
+	HistoricCustomerID uniqueidentifier NOT NULL,
+	HistoricDateEarned date NOT NULL,
+	HistoricAmount smallint NOT NULL,
+	DateMigrated date NOT NULL,
+	Expired bit NOT NULL,
+	CONSTRAINT PK_HistoricRewardID PRIMARY KEY(HistoricRewardID)
+);
